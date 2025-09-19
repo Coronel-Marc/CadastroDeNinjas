7119ac4027a924cd9a44d64271e21a42786c7ca7
Marcos <jorgede1097@gmail.com>
2025-09-19 15:40:33 -0300

    Criado o NinjaService e implementado o método de DeleteById

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -2,7 +2,10 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 
 import jakarta.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -11,27 +14,32 @@ import java.util.List;
 public class NinjaController {
 
     @Autowired
-    private NinjaRepository repository;
+    private NinjaService service;
 
     @GetMapping
     public List<NinjaModel> findAll() {
 
-        return repository.findAll();
+        return service.findNinjas();
     }
 
     @GetMapping(value = "/{id}")
     public NinjaModel findById(@PathVariable Long id) {
 
-        return repository.findById(id).get();
+        return service.findNinjaById(id);
     }
 
     @PostMapping
-    public NinjaModel insert(@RequestBody NinjaModel ninja) {
-        NinjaModel result = repository.save(ninja);
+    @ResponseStatus(HttpStatus.CREATED)
+    public NinjaModel insert(@RequestBody @Validated NinjaModel ninja) {
 
-        return result;
+        return service.novoNinja(ninja);
     }
 
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public NinjaModel deleteById(@PathVariable Long id) {
+        return service.deleteNinjaById(id);
+    }
 
 
 }
---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -0,0 +1,37 @@
+package dev.java10x.CadastroDeNinjas.Ninjas;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+public class NinjaService {
+
+    @Autowired
+    private NinjaRepository repository;
+
+
+    public NinjaModel novoNinja(NinjaModel ninja) {
+        // Regras de negócio aqui
+        return repository.save(ninja);
+    }
+
+    public List<NinjaModel> findNinjas() {
+        return repository.findAll();
+    }
+
+    public NinjaModel findNinjaById(Long id) {
+        return repository.findById(id)
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Não encontrado"));
+    }
+
+    public NinjaModel deleteNinjaById(Long id) {
+        NinjaModel ninja = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Ninja não encontrado"));
+        repository.deleteById(id);
+        return ninja;
+    }
+}
